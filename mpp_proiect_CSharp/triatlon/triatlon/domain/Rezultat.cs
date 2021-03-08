adcb7351c1c3632cc783d4ed0630e59c50a0e895
Ghera <pe.casaro10@gmail.com>
2021-03-09 00:03:20 +0200

    firstCommit

---
+++ mpp_proiect_CSharp/triatlon/triatlon/domain/Rezultat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triatlon.domain
+{
+    class Rezultat
+    {
+        public long idParticipant { get; set; }
+        public int tipProba { get; set; }
+        public int numarPuncte { get; set; }
+        public long idArbitru { get; set; }
+        public long idCompetitie { get; set; }
+        public DateTime timp_pornire { get; set; }
+
+        public DateTime timp_finalizare { get; set; }
+    }
+}
